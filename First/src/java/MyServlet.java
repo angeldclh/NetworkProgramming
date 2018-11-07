d04a908f1f81f56185b62bab811cbd7aaf2cd037
Ángel del Castillo <angel7delcastillo@gmail.com>
2018-11-07 01:39:58 +0100

    unit 10

---
+++ First/src/java/MyServlet.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author angel
+ */
+public class MyServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<h2>Welcome to my first servlet application</h2>");
+            out.println("getMethod(): " + request.getMethod() + "<BR>");
+            out.println("getRemoteAddr() :" + request.getRemoteAddr() + "<BR>");
+            out.println("getServerName(): " + request.getServerName() + "<BR>");
+            out.println("getHeader(\"Accept-Language\"): " + request.getHeader("Accept-Language") + "<BR>");
+            out.println("getParameter(\"name\"): " + request.getParameter("name") + "<BR>");
+            out.println("getParameter(\"age\"): " + request.getParameter("age") + "<BR>");
+            int n1 = Integer.parseInt(request.getParameter("n1"));
+            int n2 = Integer.parseInt(request.getParameter("n2"));
+            int res = n1 + n2;
+            out.println(n1 + " + " + n2 + " = " + res);
+            out.close();
+        }
+
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
