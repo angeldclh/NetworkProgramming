3b5ed99394553fb805e8a870c6622e4f0f0c9e51
Ángel del Castillo <angel7delcastillo@gmail.com>
2019-02-06 15:12:43 +0100

    list of conected users
    Errors: when a new user connects, the chat history is not sent only once.

--- EchoChamber/src/java/SessionHandler.java
+++ EchoChamber/src/java/SessionHandler.java
@@ -3,9 +3,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import javax.websocket.Session;
 
@@ -23,18 +20,33 @@ public class SessionHandler {
     private static final Set<Session> sessions = new HashSet<Session>();
     // "Fake database" containing lists of messages with their room ID as key. A real DB would be a way better solution
     private static final HashMap<String, ArrayList<String>> messages = new HashMap<String, ArrayList<String>>();
+    //Users in each room
+    private static final HashMap<String, ArrayList<String>> users = new HashMap<String, ArrayList<String>>();
 
     public static void addSession(Session session) {
-        //Add key with the room to hashmap if it doesn't exist
         String roomID = (String) session.getUserProperties().get("roomid");
+        String username = (String) session.getUserProperties().get("nick");
+
+        //Add user to the list
+        if (users.get(roomID) == null) { //First user in the room
+            users.put(roomID, new ArrayList<>());
+        }
+        ArrayList<String> aux = users.get(roomID);
+        aux.add(username);
+        users.put(roomID, aux);
+
+        //Add key with the room to hashmap if it doesn't exist       
         if (messages.get(roomID) == null) {
             messages.put(roomID, new ArrayList<>());
         } else {
             //Inform other users in the room about this event
-            sendToAllConnectedSessionsInRoom(roomID, "INFO: user " + session.getUserProperties().get("nick") + " has joined the room", true);
+            sendToAllConnectedSessionsInRoom(roomID, "INFO: " + username + " has joined the room", true);
         }
+
         //Actually join the room
         SessionHandler.sessions.add(session);
+        //Send the updated user list
+        sendToAllConnectedSessionsInRoom(roomID, "Users in this room: " + users.get(roomID).toString(), true);
 
         //Provide the complete history of messages since the room opening      
         messages.get(roomID).forEach((msg) -> {
@@ -43,21 +55,21 @@ public class SessionHandler {
     }
 
     public static void removeSession(Session session) {
-        SessionHandler.sessions.remove(session);
-        // Delete chat history of room if this was the last user
         String roomID = (String) session.getUserProperties().get("roomid");
-        boolean lastUser = true;
-        for (Session s : sessions) {
-            if (s.getUserProperties().get("roomid").equals(roomID)) {
-                lastUser = false;
-                break;
-            }
-        }
-        if (lastUser) {
+        String username = (String) session.getUserProperties().get("nick");
+
+        SessionHandler.sessions.remove(session);
+        users.get(roomID).remove(username);
+
+        // Delete chat history of room if this was the last user
+        if (users.get(roomID).isEmpty()) {
             messages.remove(roomID);
+            users.remove(roomID);
         } else {
             // Inform other users in the room about this event
-            sendToAllConnectedSessionsInRoom(roomID, "INFO: user " + session.getUserProperties().get("nick") + " has left the room", true);
+            sendToAllConnectedSessionsInRoom(roomID, "INFO: " + session.getUserProperties().get("nick") + " has left the room", true);
+            //Send the updated user list
+            sendToAllConnectedSessionsInRoom(roomID, "Users in this room: " + users.get(roomID).toString(), true);
         }
 
     }
@@ -80,7 +92,7 @@ public class SessionHandler {
         for (Session s : sessions) {
             if (s.getUserProperties().get("roomid").equals(roomID)) {
                 sendToSession(s, message);
-                // Insert message to "fake DB" if the message is not about the event of joining or leaving the room
+                // Insert message to "fake DB" if the message is not informative (INFO or Users in...)
                 if (!infoMessage) {
                     addMessageToRoomHistory(roomID, message);
                 }
@@ -94,4 +106,5 @@ public class SessionHandler {
         aux.add(message);
         messages.put(roomID, aux);
     }
+
 }
